1afc00661a8ba5f45190046d607fd3ca4a4c2f16
Mantenedor <wagtonazevedo@gmail.com>
2022-11-30 08:49:32 -0300

    Update 2-start.sh
    Alterando o diretório essentials...

--- scripts/2-start.sh
+++ scripts/2-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #screen -dm bash -c 'cd /opt/fivem/essenciais/ && bash /opt/fivem/essenciais/run.sh +exec server.cfg'
-screen -dm /bin/bash -c ' cd /opt/fivem/essenciais/ && ./run.sh +exec server.cfg'
+screen -dm /bin/bash -c ' cd /opt/fivem/essentials/ && ./run.sh +exec server.cfg'
